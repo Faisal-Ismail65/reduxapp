c0f3324ec1de88dbc4effdd37a65f355bc51a586
Faisal <faisalkhan7842540@gmail.com>
2022-10-11 12:39:57 -0700

    creating Navbar and shop components

---
+++ src/components/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div className='container d-flex'>
+        <button className='btn btn-primary mx-2'>-</button>
+        <p>Add This Item to Cart</p>
+        <button className='btn btn-primary mx-2'>+</button>
+    </div>
+  )
+}
+
+export default Shop
